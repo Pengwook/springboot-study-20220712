e89e4fdf48f31a79e6108175730042d3a82b9f09
LAPTOP-LJ90HDD8\khw12 <khw1204@naver.com>
2022-07-12 20:53:20 +0900

    init commit

---
+++ src/main/java/com/springboot/studyhyeonwook/StudyHyeonwookApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyhyeonwook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHyeonwookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHyeonwookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyhyeonwook/StudyHyeonwookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyhyeonwook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHyeonwookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
